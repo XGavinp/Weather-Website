058109b5fb5994c1b28309573c172d87665f5c76
XGavinp <gavinpereira2901@gmail.com>
2024-01-02 22:37:36 +0530

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
