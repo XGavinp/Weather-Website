d3bc8164e8bf3318cbfc90e9dc26c1525dc3bf15
XGavinp <gavinpereira2901@gmail.com>
2024-01-03 10:43:44 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -51,7 +51,7 @@ function App() {
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind ? <p className='bold'>{(data.wind.speed * 3.6).toFixed(2)} KPH</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
